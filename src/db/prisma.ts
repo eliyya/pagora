5c72b64adda0366400246a31d50a0a24e0992960
eliyya <tecno_eli@hotmail.com>
2026-05-02 10:37:21 -0600

    lint

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,22 +1,22 @@
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from './generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient;
-  prismaAdapter: PrismaPg;
-};
+    prisma: PrismaClient
+    prismaAdapter: PrismaPg
+}
 const adapter =
-  globalForPrisma.prismaAdapter ?? new PrismaPg(process.env.DATABASE_URL!);
+    globalForPrisma.prismaAdapter ?? new PrismaPg(process.env.DATABASE_URL!)
 
 const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        adapter,
+    })
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prismaAdapter = adapter;
-  globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prismaAdapter = adapter
+    globalForPrisma.prisma = db
 }
 
-export { db };
+export { db }
