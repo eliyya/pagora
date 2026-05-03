f47207eae7344b423d5f6964d52a3cd2164080d0
eliyya <tecno_eli@hotmail.com>
2026-05-02 20:05:00 -0600

    lint

--- src/app/chargues-table.tsx
+++ src/app/chargues-table.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { MoreHorizontalIcon, PlusIcon } from 'lucide-react'
+import { MoreHorizontalIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import {
