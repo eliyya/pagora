7407514e42c4b29c5db4478d0309edc9a2d8ea8c
mael098 <juanvillegasmorales@outlook.com>
2026-05-03 22:32:09 -0600

    agrege export  en REDIRECT_PATH

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,6 @@ export const COOKIES = {
     REFRESH: 'refresh',
 } as const
 
-const REDIRECT_PATH = '/api/auth/login'
+export const REDIRECT_PATH = '/api/auth/login'
 
 export const DISCORD_URL_REDIRECT = `${APP_URL}${REDIRECT_PATH}`
